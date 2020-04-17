ed5d14f06c44b6c1fd4bf120954fce9ac85dedbd
Alex Thibodeau <alexthibodeau@unity3d.com>
2020-04-17 16:27:26 -0400

    Initial commit at bringing unitytls to upstream mono. Currently does not build!

--- mcs/class/System/Mono.Net.Security/MonoTlsProviderFactory.cs
+++ mcs/class/System/Mono.Net.Security/MonoTlsProviderFactory.cs
@@ -212,6 +212,7 @@ namespace Mono.Net.Security
 
 #endregion
 
+		internal static readonly Guid UnityTlsId = new Guid("06414A97-74F6-488F-877B-A6CA9BBEB82E");
 		internal static readonly Guid AppleTlsId = new Guid ("981af8af-a3a3-419a-9f01-a518e3a17c1c");
 		internal static readonly Guid BtlsId = new Guid ("432d18c9-9348-4b90-bfbf-9f2a10e1f15b");
 
@@ -226,9 +227,26 @@ namespace Mono.Net.Security
 				providerRegistration = new Dictionary<string,Tuple<Guid,string>> ();
 				providerCache = new Dictionary<Guid,MobileTlsProvider> ();
 
+				// Let Unity check to see if we have an implementation first before falling back to the usual behavior
+#if UNITY
+				if (Mono.Unity.UnityTls.IsSupported) {
+					PopulateUnityProviders ();
+					return;
+				}
+#endif
+
 				PopulateProviders ();
 			}
 		}
+
+#if UNITY
+		static void PopulateUnityProviders ()
+		{
+			var unityTlsEntry = new Tuple<Guid,String> (UnityTlsId, "Mono.Unity.UnityTlsProvider");
+			providerRegistration.Add ("default", unityTlsEntry);
+			providerRegistration.Add ("unitytls", unityTlsEntry);
+		}
+#endif
 
 #if ONLY_APPLETLS || MONOTOUCH || XAMMAC
 		// TODO: Should be redundant
